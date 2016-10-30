a81401b7fbac0c15249ec0a237a3f28f37d50a06
David Rielo <david.rielo@24imedia.com>
2016-10-30 21:15:35 +0100

    ES6 with babel and gulp

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const del = require('del');
+const exec = require('child_process').exec;
+
+const paths = {
+  allSrcJs: 'src/**/*.js',
+  libDir: 'lib',
+};
+
+// recommended way to delete files with gulphttps://github.com/gulpjs/gulp/blob/master/docs/recipes/delete-files-folder.md
+gulp.task('clean', () => {
+  return del(paths.libDir);
+});
+
+gulp.task('build', ['clean'], () => {
+  return gulp.src(paths.allSrcJs)
+    .pipe(babel())
+    .pipe(gulp.dest(paths.libDir));
+});
+
+// like running node lib
+gulp.task('main', ['build'], (callback) => {
+  exec(`node ${paths.libDir}`, (error, stdout) => {
+    console.log(stdout);
+    return callback(error);
+  });
+});
+
+// runs the main task when filesystem changes happen in the specified files
+gulp.task('watch', () => {
+  gulp.watch(paths.allSrcJs, ['main']);
+});
+
+gulp.task('default', ['watch', 'main']);
+
+ /* 
+ * You might be wondering how come we're using some ES6 code in this Gulp file, since it doesn't get transpiled into ES5 by Babel. 
+ * This is because we're using a version of Node that supports ES6 features out of the box (make sure you are running Node > 6.5.0 by running node -v).
+ * /
---
+++ src/index.js
@@ -0,0 +1,4 @@
+const str = 'ES6';
+
+// template strings are created using backquotes
+console.log(`Hello ${str}`);
